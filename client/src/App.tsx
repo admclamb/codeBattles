1cae41d3074819d85c28b28808699454e4f937f1
admclamb <adylanmclamb@yahoo.com>
2022-07-30 16:49:15 -0400

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,13 @@
 import PageRoutes from './pages/PageRoutes';
 import Layout from './components/Layout/Layout';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: '#4f5b62',
-      main: '#263238',
-      dark: '#000a12',
-    },
-    secondary: {
-      light: '#8eacbb',
-      main: '#607d8b',
-      dark: '#34515',
-    },
-    text: {
-      primary: '#fff',
-      secondary: '#000',
-    },
-  },
-});
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
+    <div className="bg-dark">
         <Layout>
-          <h1>Yo</h1>
+          <button className="btn btn-primary">Yo</button>
         </Layout>
-      </ThemeProvider>
-    </>
+    </div>
   );
 }
 
