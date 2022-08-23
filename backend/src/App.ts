17300a245c61e1c27088985e7c7a469d18ddf640
admclamb <adylanmclamb@yahoo.com>
2022-08-23 00:27:11 -0400

    update

---
+++ backend/src/App.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { errorHandler } from './errors/errorHandler';
+import { notFound } from './errors/notFound';
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(notFound);
+app.use(errorHandler);
--- client/src/components/navigation/Navbar/Navbar.tsx
+++ client/src/components/navigation/Navbar/Navbar.tsx
@@ -3,23 +3,6 @@ import { Link } from 'react-router-dom';
 type Props = {};
 
 const Navbar = (props: Props) => {
-  return (
-    <nav className='side-navbar bg-darker text-light'>
-      <Link to="/" className="navbar-brand">
-        Code Battles
-      </Link>
-      <ul>
-        <li>
-          <Link to="/battle"><i className="fa-solid fa-puzzle-piece fa-3x"></i><p>Code Rush</p></Link>
-        </li>
-        <li>
-          <Link to="/battle"><i className="fa-solid fa-axe-battle fa-3x"></i><p>Battle</p></Link>
-        </li>
-        <li>
-          <Link to="/battle">Daily Challenge</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  return <nav className="bg-darker text-light"></nav>;
 };
 export default Navbar;
