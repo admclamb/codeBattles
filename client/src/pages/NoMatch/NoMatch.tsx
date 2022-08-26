e10209dd23a2a5528c927dcd7f86c6d9d626b1eb
admclamb <adylanmclamb@yahoo.com>
2022-08-25 20:59:27 -0400

    update not found

--- client/src/pages/NoMatch/NoMatch.tsx
+++ client/src/pages/NoMatch/NoMatch.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
-type Props = {};
-
-const NoMatch = (props: Props) => {
-  return <div>NoMatch</div>;
+import Card from '../../components/card/Card/Card';
+import { Link } from 'react-router-dom';
+import styles from './NoMatch.module.css';
+const NoMatch = () => {
+  return (
+    <div className={`${styles.container} container`}>
+      <Card>
+        <h2>404 Page not found</h2>
+        <p className={styles.description}>
+          We cannot find the page you are looking for
+        </p>
+        <Link to="/" className="button button-primary">
+          Homepage
+        </Link>
+      </Card>
+    </div>
+  );
 };
 
 export default NoMatch;
--- client/src/pages/Puzzle/Puzzle.tsx
+++ client/src/pages/Puzzle/Puzzle.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Card from '../../components/card/Card/Card';
 type Props = {};
 
 const Puzzle = (props: Props) => {
   return (
     <div className="container">
       <h1>Puzzle Battle</h1>
-      <Card></Card>
+      <Card>{<h1>Puzzle</h1>}</Card>
     </div>
   );
 };
